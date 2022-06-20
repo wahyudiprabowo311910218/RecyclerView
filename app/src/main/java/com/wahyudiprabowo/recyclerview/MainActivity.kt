976ff0eee8a52b2370cfd5d121f804ab794856e8
WahyudiPrabowo311910218 <wahyudiprabowo311910218>
2022-06-20 19:28:21 +0100

    Recyclerview APP

--- app/src/main/java/com/wahyudiprabowo/recyclerview/MainActivity.kt
+++ app/src/main/java/com/wahyudiprabowo/recyclerview/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.wahyudiprabowo.recyclerview
 
+import android.app.Dialog
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import com.wahyudiprabowo.recyclerview.adapter.AdapterTeamBola
 import com.wahyudiprabowo.recyclerview.databinding.ActivityMainBinding
 import com.wahyudiprabowo.recyclerview.model.Pemain
@@ -24,7 +30,30 @@ class MainActivity : AppCompatActivity() {
 
         binding.list.adapter = AdapterTeamBola( this,listPemain, object  : AdapterTeamBola.OnClickListener {
             override fun detailData(item: Pemain?) {
-                TODO("Not yet implemented")
+                Dialog(this@MainActivity).apply {
+                    requestWindowFeature(Window.FEATURE_NO_TITLE)
+                    setCancelable(true)
+                    setContentView(R.layout.detail_data_pemain)
+
+                    val image = this.findViewById<ImageView>(R.id.image_pemain)
+                    val nama = this.findViewById<TextView>(R.id.txt_namapemain)
+                    val posisi = this.findViewById<TextView>(R.id.txt_posisi)
+                    val tinggi = this.findViewById<TextView>(R.id.txt_tinggibadan)
+                    val tempatlahir = this.findViewById<TextView>(R.id.txt_tempatlahir)
+                    val tgllahir = this.findViewById<TextView>(R.id.txt_tanggallahir)
+                    val btn = this.findViewById<Button>(R.id.btnClose)
+
+                    image.setImageResource(item?.foto ?:0)
+                    nama.text = "${item?.nama}"
+                    posisi.text = "${item?.posisi}"
+                    tinggi.text = "${item?.tinggi}"
+                    tempatlahir.text = "${item?.tempatlahir}"
+                    tgllahir.text = "${item?.tgllahir}"
+
+                    btn.setOnClickListener{
+                        this.dismiss()
+                    }
+                }.show()
             }
 
 
